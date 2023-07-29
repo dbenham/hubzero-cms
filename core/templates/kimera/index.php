f543a4b3e82e0ea01b1cab196bcabb83b4839be8
David Benham <david.r.benham@gmail.com>
2023-07-28 20:13:40 -0400

    Update index.php

--- core/templates/kimera/index.php
+++ core/templates/kimera/index.php
@@ -75,7 +75,7 @@ if ($this->getTitle() != Config::get('sitename'))
 							<jdoc:include type="modules" name="notices" />
 
 							<h1>
-							808 Test
+							813 Test
 							</h1>
 
 							<nav id="account" class="account-navigation">
